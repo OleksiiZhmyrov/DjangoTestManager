b05bd81ecc782d5d06fc4a35c8d1bba507f6838a
Oleksii Zhmyrov <zhmyrov@gmail.com>
2014-11-04 10:27:50 +0200

    Attempt to add user email address while registering.

--- TestManagerCore/views.py
+++ TestManagerCore/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.generic import View, CreateView, UpdateView
 
 from TestManagerCore.forms import UserRegistrationForm, UserProfileUpdateForm
+from django.core.validators import validate_email
 
 
 class UserLogOutView(View):
@@ -24,6 +26,11 @@ class UserRegistrationView(CreateView):
 
     def form_valid(self, form):
         form.instance.is_active = False
+        try:
+            validate_email(form.instance.username)
+            form.instance.email = form.instance.username
+        except ValidationError:
+            pass
         return super(UserRegistrationView, self).form_valid(form)
 
 
